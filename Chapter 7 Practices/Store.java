14f457969b24e4c99b6fc1b37e2c32b796161b9c
CrugXC <jtrixie@NN123-HA-06.HS.sd203.org>
2015-12-11 14:46:54 -0600

    Practices

--- Chapter 7 Practices/Store.java
+++ Chapter 7 Practices/Store.java
@@ -3,12 +3,31 @@ import java.util.Scanner;
 
 public class Store
 {
-
     public static void main(String[] args)
     {
-       System.out.println("Insert End for Customer Name to end program.");
+       System.out.println("Enter End for Customer Name to end program.");
        
+       String name;
+       double amount;
+       TopCustomers topCust = new TopCustomers();
+       Scanner s1 = new Scanner(System.in);
        
+       do
+       {
+           System.out.print("Input customer name: ");
+           
+           name = s1.next();
+           
+           if ((name.equals("End") || name.equals("end")) == false)
+           {
+               System.out.print("Input amount: ");
+               
+               amount = s1.nextDouble();
+               
+               topCust.addSale(name, amount);
+           }
+        } while((name.equals("End") || name.equals("end")) == false);
+       
+       System.out.println("The top customer is " + topCust.nameOfBestCustomer() + "!");
     }
-
 }
